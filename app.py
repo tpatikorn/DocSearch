a4985d45625f0e35a0ca38b52e535b1d10098fcf
tpatikorn <tpatikorn@gmail.com>
2023-12-15 18:16:44 +0700

    add option to search from different ocr engines

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
 from typing import List
 from pythainlp import tokenize
-from flask import Flask, request, render_template, url_for, send_from_directory
+from flask import Flask, request, render_template, url_for, send_from_directory, jsonify
 import pandas as pd
 
-text_location = "text/summary.csv"
 app = Flask(__name__)
+
+TESSERACT = "tesseract"
+EASYOCR = "easyocr"
+THA_ENG = "tha+eng"
+THA = "tha"
+
+
+def get_text_location(ocr_engine: str, lang: str) -> str:
+    return f"text/summary_{ocr_engine}_{lang}.csv"
 
 
 def _search(query: str, text: pd.DataFrame, title_only: bool, use_tokenizer: bool) -> pd.DataFrame:
@@ -13,8 +21,7 @@ def _search(query: str, text: pd.DataFrame, title_only: bool, use_tokenizer: boo
         if title_only:
             return text[query in text["filename"]]
         else:
-            return text[query in text["filename"] |
-                        query in text["text"]]
+            return text[query in text["text"]]
     else:
         terms: List[str] = tokenize.word_tokenize(query)
         if title_only:
@@ -30,16 +37,31 @@ def _search(query: str, text: pd.DataFrame, title_only: bool, use_tokenizer: boo
 @app.route("/search")
 def search():
     query: str = request.args.get('query')
-    text = pd.read_csv(text_location, dtype={"filename": "string", "text": "string"})
+    ocr_engine = (request.args.get('ocr_engine') or TESSERACT).lower()
+    lang = (request.args.get('lang') or THA_ENG).lower()
+    text = pd.read_csv(get_text_location(ocr_engine, lang), dtype={"filename": "string", "text": "string"})
     title_only: bool = (request.args.get('title_only') or "false").lower() == "true"
     use_tokenizer: bool = (request.args.get('use_tokenizer') or "true").lower() == "true"
-    aggregate : bool = (request.args.get('aggregate') or "true").lower() == "false"
+    aggregate: bool = (request.args.get('aggregate') or "true").lower() == "false"
     text['text'] = text['filename'].astype(str) + ' ' + text['text']
+    print(ocr_engine, lang, title_only, use_tokenizer, aggregate)
     if aggregate:
         text = text.groupby(['filename', 'relative_path']).agg({'text': lambda x: ' '.join(x)}).reset_index()
         text['page'] = 0
 
     return _search(query, text, title_only, use_tokenizer).to_json()
+
+
+@app.route("/search_compare")
+def search_compare():
+    query: str = request.args.get('query')
+    results = {}
+    conditions = [(TESSERACT, THA_ENG), (TESSERACT, THA), (EASYOCR, THA_ENG), (EASYOCR, THA)]
+    for engine, lang in conditions:
+        text = pd.read_csv(get_text_location(engine, lang), dtype={"filename": "string", "text": "string"})
+        text['text'] = text['filename'].astype(str) + ' ' + text['text']
+        results[(engine, lang)] = _search(query, text, False, True).to_json()
+    return jsonify(results)
 
 
 @app.route("/")
